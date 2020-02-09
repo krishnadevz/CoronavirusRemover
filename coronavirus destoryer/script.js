1a5d57ed2866f33e4fb4bdbedaa28bae494f3a18
krishna kakade <krishnakakade77@gmail.com>
2020-02-09 10:16:02 +0530

    final commit

---
+++ coronavirus destoryer/script.js
@@ -0,0 +1,18 @@
+
+replaceText(document.body)
+
+function replaceText(element){
+if (element.hasChildNodes()){
+    element.childNodes.forEach(replaceText)
+ } else if (element.nodeType===Text.TEXT_NODE){
+     if(element.textContent.match(/coronavirus/gi)){
+       //  const newElement=document.createElement('span')
+        // newElement.innerHTML=element.textContent.replace(/(coronavirus)/gi,
+         //   '<span style="background-color:black; color:black;">$1</span>')
+      //  element.replaceWith(newElement)
+      element.parentElement.remove()
+    }
+//element.textContent=element.textContent.replace(/coronvirus/gi,'')
+
+ }
+}
